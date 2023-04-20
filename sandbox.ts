d53ed369915693e031ad1d1be6ea74b375e04b68
Victoria Holub <victoria_holub@epam.com>
2023-04-20 11:09:10 +0300

    lesson-3: type basics

--- sandbox.ts
+++ sandbox.ts
@@ -1,8 +1,18 @@
-const character = "mario";
-console.log(character);
-const inputs = document.querySelectorAll("input");
-console.log(inputs);
+let character = "mario";
+let age = 30;
+let isBlackBelt = false;
 
-inputs.forEach((input) => {
-  console.log(input);
-});
+// character = 20 // Error
+character = "luigi";
+
+// age = 'yoshi' // Error
+age = 40;
+
+// isBlackBelt = 'yes' // Error
+isBlackBelt = true;
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+console.log(circ(5.5));
