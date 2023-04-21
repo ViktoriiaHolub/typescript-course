54945d67d66848f040b17ac1aaae000a01e4c21c
Victoria Holub <victoria_holub@epam.com>
2023-04-21 16:27:02 +0300

    lesson-8: function basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log("test");
+let greet;
+// greet = 'hello'
+greet = () => {
+    console.log("hello");
+};
+const sum = (a, b, c = 10, d) => {
+    console.log(a + b + c);
+    console.log(d);
+};
+sum(10, 23, 4);
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 4);
+result = 5;
+// result = 'hello' // Error
