ed03e17e485a4c7f591c13e8f06020dc1972a8be
Victoria Holub <victoria_holub@epam.com>
2023-04-24 14:47:11 +0300

    lesson-16: Interfaces with classes

--- public/app.js
+++ public/app.js
@@ -1,17 +1,38 @@
-"use strict";
-// interfaces
-const me = {
-    name: "Vika",
-    age: 27,
-    speak(text) {
-        console.log(text);
-    },
-    spend(count) {
-        console.log(count);
-        return count;
-    },
-};
-const greetPerson = (person) => {
-    console.log("hello,", person.name);
-};
-greetPerson(me);
+import { Invoice } from "./classes/invoice.js";
+import { Payment } from "./classes/payment.js";
+const invOne = new Invoice("Mario", "working", 200);
+const invTwo = new Invoice("Luigi", "working", 1000);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+// invOne.client = "Yoshi"; // Error
+invTwo.amount = 400;
+// invTwo.details = 'resting' // Error
+console.log(invOne, invTwo);
+invoices.forEach((invoice) => {
+    console.log(`${invoice.client} currently has ${invoice.amount}. ${invoice.format()}`);
+});
+// DOM
+const anchor = document.querySelector("a");
+// if (anchor) {
+//   console.log(anchor.href);
+// }
+console.log(anchor.href);
+const form = document.querySelector(".new-item-form");
+console.log(form.children);
+// type of inputs
+const type = document.querySelector("#type");
+const toFrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let filledForm;
+    if (type.value === "Payment") {
+        filledForm = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        filledForm = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log("filledForm", filledForm);
+});
