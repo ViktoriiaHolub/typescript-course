f3e39a673843596e0ac68f636f6c584ee01f9acf
Victoria Holub <victoria_holub@epam.com>
2023-04-24 13:29:25 +0300

    lesson-14: modules

--- public/app.js
+++ public/app.js
@@ -1,18 +1,4 @@
-"use strict";
-// classes
-class Invoice {
-    // readonly client: string;
-    // private details: string;
-    // public amount: number;
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from "./classes/invoice.js";
 const invOne = new Invoice("Mario", "working", 200);
 const invTwo = new Invoice("Luigi", "working", 1000);
 let invoices = [];
